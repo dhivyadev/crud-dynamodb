a0ef92710872cf49422b98a4b34b10e32b288f97
boopathyganesh <boopathyganesh95@gmail.com>
2023-08-09 02:12:35 +0530

    pushing sortkey(SK) as dictionary item

--- dynamodb_crud.js
+++ dynamodb_crud.js
@@ -1,43 +1,40 @@
-const { DynamoDBClient, CreateTableCommand, DescribeTableCommand } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBClient, CreateTableCommand, DescribeTableCommand, PutItemCommand } = require("@aws-sdk/client-dynamodb");
+
+const dynamoDBEndpoint = "http://localhost:8000";
+const region = "us-east-1"; // Change to your desired region
+const tableName = "Value1";
 
 async function createTable() {
-  // Configure the AWS SDK with your credentials and region
-  const client = new DynamoDBClient({ region: "us-east-2" }); // Replace "us-east-2" with your desired region
-
-  // Specify your desired table name
-  const tableName = "YourTableName";
+  const client = new DynamoDBClient({ region, endpoint: dynamoDBEndpoint });
 
   try {
-    // Check if the table already exists by describing the table
     const describeTableCommand = new DescribeTableCommand({ TableName: tableName });
     await client.send(describeTableCommand);
     console.log(`Table "${tableName}" already exists.`);
   } catch (error) {
-    // If the table does not exist, create it
     if (error.name === "ResourceNotFoundException") {
-      // Define the attribute definitions for the table
+      // const SK =[
+      //   { AttributeName: "Zoho", AttributeType: "S" },
+      //   { AttributeName: "AUGMONT", AttributeType: "S" },
+      //   { AttributeName: "NewToken", AttributeType: "S" },
+      //   { AttributeName: "AUGMONT#GOLD_SILVER_RATES", AttributeType: "S" },
+      //   { AttributeName: "AUGMONT#PASSBOOK", AttributeType: "S" }
+      // ]
       const attributeDefinitions = [
-        { AttributeName: "GlobalKeys", AttributeType: "S" },
-        { AttributeName: "Zoho", AttributeType: "S" },
-        { AttributeName: "Augmont", AttributeType: "S" },
-        { AttributeName: "newToken", AttributeType: "S" },
+        { AttributeName: "PK", AttributeType: "S" },
+        { AttributeName: "SK", AttributeType: "S" },
       ];
 
-      // Define the key schema for the table
       const keySchema = [
-        { AttributeName: "GlobalKeys", KeyType: "HASH" }, // Partition key (PK)
-        { AttributeName: "Zoho", KeyType: "RANGE" }, // Sort key (SK) - Part of the composite sort key
-        { AttributeName: "Augmont", KeyType: "RANGE" }, // Sort key (SK) - Part of the composite sort key
-        { AttributeName: "newToken", KeyType: "RANGE" }, // Sort key (SK) - Part of the composite sort key
+        { AttributeName: "PK", KeyType: "HASH" },
+        { AttributeName: "SK", KeyType: "RANGE" },
       ];
 
-      // Define the provisioned throughput (change as per your requirements)
       const provisionedThroughput = {
         ReadCapacityUnits: 5,
         WriteCapacityUnits: 5,
       };
 
-      // Create the table command
       const createTableCommand = new CreateTableCommand({
         TableName: tableName,
         AttributeDefinitions: attributeDefinitions,
@@ -46,76 +43,71 @@ async function createTable() {
       });
 
       try {
-        // Send the CreateTable command to AWS DynamoDB service
         const data = await client.send(createTableCommand);
         console.log("Table created successfully:", data);
       } catch (createError) {
         console.error("Error creating table:", createError);
       }
     } else {
-      // For any other error, print the error message
       console.error("Error describing table:", error);
     }
   }
 }
-// Call the createTable function to create the table or show if it already exists
-createTable();
 
 async function putDataIntoTable() {
-    // Configure the AWS SDK with your credentials and region
-    const client = new DynamoDBClient({ region: "us-east-2" }); // Replace "us-east-2" with your desired region
-  
-    // Specify your table name
-    const tableName = "YourTableName";
-  
-    // Sample dummy data to be inserted
-    const dummyData = [
+  const client = new DynamoDBClient({ region, endpoint: dynamoDBEndpoint });
+  const data = {
+    Zoho: [
       {
-        GlobalKeys: "key1",
-        Zoho: "zoho1",
-        Augmont: "augmont1",
-        newToken: "newtoken1",
-        Bearer_Token: "bearer1",
-        Key: "key1",
-        Value: "value1",
-      },
-      {
-        GlobalKeys: "key2",
-        Zoho: "zoho2",
-        Augmont: "augmont2",
-        newToken: "newtoken2",
-        Bearer_Token: "bearer2",
-        Key: "key2",
-        Value: "value2",
-      },
-      // Add more dummy data items as needed
-    ];
-  
-    // Insert each item in the dummy data array into the table
-    for (const item of dummyData) {
-      // Create the PutItem command for each item
-      const command = new PutItemCommand({
-        TableName: tableName,
-        Item: {
-          GlobalKeys: { S: item.GlobalKeys },
-          Zoho: { S: item.Zoho },
-          Augmont: { S: item.Augmont },
-          newToken: { S: item.newToken },
-          Bearer_Token: { S: item.Bearer_Token },
-          Key: { S: item.Key },
-          Value: { S: item.Value },
-        },
-      });
-  
-      try {
-        // Send the PutItem command to AWS DynamoDB service to write the data
-        const data = await client.send(command);
-        console.log(`Data inserted for ${item.GlobalKeys} successfully:`, data);
-      } catch (error) {
-        console.error(`Error inserting data for ${item.GlobalKeys}:`, error);
+        "Bearer-Token": { S: "" },
+        "Key": { S: "" },
+        "Value": { S: "" }
       }
+    ],
+    AUGMONT: [
+      {
+        "Bearer-Token": { S: "" },
+        "Key": { S: "" },
+        "Value": { S: "" }
+      }
+    ],
+    NewToken: [
+      {
+        "Bearer-Token": { S: "" },
+        "Key": { S: "" },
+        "Value": { S: "" }
+      }
+    ]
+  };
+  const dummyData = [
+    {
+      PK: "Global_Keys",
+      SK: JSON.stringify(data),
+    }
+    // Add more dummy data items as needed
+  ];
+
+  for (const item of dummyData) {
+    const command = new PutItemCommand({
+      TableName: tableName,
+      Item: {
+        PK: { S: item.PK },
+        SK: { S: item.SK }
+      }
+    });
+
+    try {
+      const data = await client.send(command);
+      console.log(`Data inserted for ${item.PK} successfully:`, data);
+    } catch (error) {
+      console.error(`Error inserting data for ${item.PK}:`, error);
     }
   }
-  
-  // Call the putDataIntoTable function to insert dummy data into the table
-  putDataIntoTable();
+}
+
+async function main() {
+  //await createTable();
+  await putDataIntoTable();
+}
+
+main();
